4cc2340cb66419d58d013839524a9e25ac704977
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-21 19:27:45 -0400

    Create process request function and fix crlf

--- HTTPConnectionWorkerThread.java
+++ HTTPConnectionWorkerThread.java
@@ -10,24 +10,32 @@ public class HTTPConnectionWorkerThread extends Thread {
         this.socket = socket;
     }
 
+    public static String processRequest(String request) {
+        return "hey";
+    }
+
     @Override
     public void run() {
         try {
             InputStream inputStream = socket.getInputStream();
             OutputStream outputStream = socket.getOutputStream();
 
+            String request = "GET /api/books HTTP/1.1\r\nHost: www.example.com\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36\r\nAccept: application/json\r\n\r\n";
+            String result = processRequest(request);
+
+
             // TODO we would write
             String html = "<html><head><title>Simple HTTP Server</title></head><body><h1>Hello World 1</h1></body></html>";
 
-            final String CRLF = "\n\r"; // 13, 10 in ASCII
+            final String CRLF = "\r\n"; // 13, 10 in ASCII
             // Need to wrap string in HTTP response
             String response =
                 "HTTP/1.1 200 OK" + CRLF +
-                "Content-Length: " + html.getBytes().length + CRLF + CRLF +
-                html + CRLF + CRLF;
+                "Content-Length: " + result.getBytes().length + CRLF + CRLF +
+                result + CRLF + CRLF;
             
             System.out.println("Prepared");
-            sleep(5000);
+            // sleep(5000);
             System.out.println("Returning value");
 
             outputStream.write(response.getBytes());
@@ -37,8 +45,6 @@ public class HTTPConnectionWorkerThread extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
             // TODO HANDLE LATER
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 }
