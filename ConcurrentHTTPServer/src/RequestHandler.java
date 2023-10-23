2b3d771b8727ea5e5726fe0a402daa5f788b867d
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-23 10:47:58 -0400

    Invalid or insecure path checking

--- ConcurrentHTTPServer/src/RequestHandler.java
+++ ConcurrentHTTPServer/src/RequestHandler.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -83,29 +87,42 @@ public class RequestHandler {
         int port = 8080;
 
         String documentRoot = serverConfig.getRootFrom(requestMap.get("Host"), port);
-        if (requestMap.get("Host") == null)
+        if (requestMap.get("Host") == null || requestMap.get("Host").startsWith("localhost"))
             documentRoot = serverConfig.getFirstRoot(port);
-            
-        System.out.println(documentRoot);
 
         if (documentRoot == null)
             throw new ResponseException("Host " + requestMap.get("Host") + " could not be resolved", 404);
 
-        String baseDirectory = "../../" + documentRoot;
-        File res;
-        System.out.println(baseDirectory + "/index.html");
+        String uri = getURI(path, documentRoot);
+        File res = new File(uri);
+
 
         // TODO: MIME checking
-        // TODO: security attack handling (../)
-
-        if (path.equals("/")) {
-            res = new File(baseDirectory + "/index.html");
-        } else {
-            res = new File(baseDirectory + path);
-        }
 
         if (!res.exists()){
             throw new ResponseException(path + " could not be found", 404);
+        }
+
+        return res;
+    }
+
+    private String getURI(String path, String documentRoot) throws ResponseException{
+        String res = "";
+
+        if ((documentRoot + path).contains("../") || (documentRoot + path).endsWith("/..") || (documentRoot + path).equals(".."))
+            throw new ResponseException(path + " is not a valid path", 404);
+
+        if (path.equals("/")) {
+            res = "../../" + documentRoot + "/index.html";
+        } else {
+            res = "../../" + documentRoot + path;
+        }
+
+        // Check for malformed path
+        try {
+            new URI(res);
+        } catch (URISyntaxException e) {
+            throw new ResponseException(path + " is not a valid path", 404);
         }
 
         return res;
